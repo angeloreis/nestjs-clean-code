42b2ef0e2208e3f5670c75f7399a31f6d6cbd8b5
Angelo Reis <angelo.desenvolvedor@gmail.com>
2023-08-17 21:15:16 -0300

    chore: implements test and config run script

---
+++ test/unit/shared/infrastructure/env-config.service.spec.ts
@@ -0,0 +1,28 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { EnvConfigService } from '@/shared/infrastructure/env-config/env-config.service'
+import { EnvConfigModule } from '@/shared/infrastructure/env-config/env-config.module'
+
+describe('EnvConfigService unit tests', () => {
+  let systemUnderTest: EnvConfigService
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [EnvConfigModule.forRoot()],
+      providers: [EnvConfigService],
+    }).compile()
+
+    systemUnderTest = module.get<EnvConfigService>(EnvConfigService)
+  })
+
+  it('should be defined', () => {
+    expect(systemUnderTest).toBeDefined()
+  })
+
+  it('should be return a PORT of application', () => {
+    expect(systemUnderTest.getAppPort()).toBe(3001)
+  })
+
+  it('should be return NODE ENV of application', () => {
+    expect(systemUnderTest.getNodeEnv()).toBe('test')
+  })
+})
