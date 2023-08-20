8676fab2caab0683c21dc74f19d11a4c007e43e5
Angelo Reis <angelo.desenvolvedor@gmail.com>
2023-08-20 18:56:45 -0300

    fix(user): entity of user extend Entity abs class

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,3 +1,5 @@
+import { Entity } from '@/shared/domain/entities/entity'
+
 export type UserProps = {
   name: string
   email: string
@@ -5,13 +7,34 @@ export type UserProps = {
   createdAt?: Date
 }
 
-export class UserEntity {
-  constructor(public readonly props: UserProps) {
+export class UserEntity extends Entity<UserProps> {
+  constructor(public readonly props: UserProps, id?: string) {
+    super(props, id)
     this.props.createdAt = this.props.createdAt ?? new Date()
+  }
+
+  update(value: string): void {
+    this.setName(value)
+  }
+
+  updatePassword(value: string): void {
+    this.setPassword(value)
   }
 
   getName() {
     return this.props.name
+  }
+
+  private setName(value: string) {
+    this.props.name = value
+  }
+
+  getPassword() {
+    return this.props.password
+  }
+
+  private setPassword(value: string) {
+    this.props.password = value
   }
 
   getEmail() {
